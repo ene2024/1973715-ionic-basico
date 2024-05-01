1bb0471bf0f15216c0ca593baee407114526a340
aguilar036 <cristianantonio411@gmail.com>
2024-04-30 19:23:46 -0600

    Practica 12: Acceso a Camara

--- miApp-1973715/src/app/app.module.ts
+++ miApp-1973715/src/app/app.module.ts
@@ -11,10 +11,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { ProductosComponent } from './productos/productos.component';
 import { ProductoDetalleComponent } from './producto-detalle/producto-detalle.component';
 import { ListaAlumnosComponent } from './lista-alumnos/lista-alumnos.component';
+import { CamaraComponent } from './camara/camara.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, ProductosComponent, ProductoDetalleComponent, ListaAlumnosComponent ],
+  declarations: [AppComponent, ProductosComponent, ProductoDetalleComponent, ListaAlumnosComponent, CamaraComponent ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule, FormsModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
