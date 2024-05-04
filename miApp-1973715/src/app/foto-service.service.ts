cbd9a933c5c5e8d3ae5ed72500cd24181957677b
Aguilar036 <cristianantonio411@gmail.com>
2024-05-03 23:11:30 -0600

    Practica 12: Acceso a Recursos - Parte 2

--- miApp-1973715/src/app/foto-service.service.ts
+++ miApp-1973715/src/app/foto-service.service.ts
@@ -2,12 +2,14 @@ import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
 import { Preferences } from '@capacitor/preferences';
+import { Foto } from './modules/foto.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FotoServiceService {
 
+  public fotos: Foto[] = [];
   constructor() { }
 
   public async addNewToGallery() {    
@@ -17,6 +19,11 @@ export class FotoServiceService {
       source: CameraSource.Camera,    
       quality: 100    
     });
+
+    this.fotos.unshift({
+      filepath: '',
+      webViewPath: capturedPhoto.webPath
+    })
   }
 }
 
